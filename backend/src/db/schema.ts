acccb42e698b61959fde445daddbc09bb0c1ad22
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-05-05 23:59:18 +0100

    setup drizzle halfway

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,6 @@
+import { pgTable, integer } from "drizzle-orm/pg-core"
+
+// schema guide
+// export  const table name in ts = pgTable(table name in database , {
+//     column name in ts : database type ( db column name )
+// })
