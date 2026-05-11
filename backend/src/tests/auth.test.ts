7e45913390d787a0696b0fd0fef806cd22194d3c
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-05-11 22:54:55 +0100

    configure register-customer endpoint test

--- backend/src/tests/auth.test.ts
+++ backend/src/tests/auth.test.ts
@@ -1,5 +1,29 @@
-describe('sanity check', () => {
-  it('should pass', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
+import express from "express";
+import request from "supertest";
+import app from "../app.js";
+
+const router = express.Router();
+
+jest.setTimeout(15000); 
+
+describe("POST /auth/register-customer", () => {
+  it("returns status code 201 if first name is passed", async () => {
+    const res = await request(app)
+      .post("/auth/register-customer")
+      .send({
+        first_name: "john",
+        last_name: "doe",
+        email: "john.doe@example.com",
+        password: "password123",
+      });
+    expect(res.statusCode).toEqual(201);
+    expect(res.body.data.role).toEqual("customer");
+  });
+});
+
+
+// describe('sanity check', () => {
+//   it('should pass', () => {
+//     expect(1 + 1).toBe(2)
+//   })
+// })
