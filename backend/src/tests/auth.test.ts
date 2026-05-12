e7a63f6485244ddfcaadb0ac21beb1165cf2cbfb
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-05-12 01:15:54 +0100

    test:implement customer login test

--- backend/src/tests/auth.test.ts
+++ backend/src/tests/auth.test.ts
@@ -6,21 +6,34 @@ const router = express.Router();
 
 jest.setTimeout(15000); 
 
-describe("POST /auth/register-customer", () => {
-  it("returns status code 201 if first name is passed", async () => {
+// describe("POST /auth/register-customer", () => {
+//   it("returns status code 201 if first name is passed", async () => {
+//     const res = await request(app)
+//       .post("/auth/register-customer")
+//       .send({
+//         first_name: "john",
+//         last_name: "doe",
+//         email: "john.doe@example.com",
+//         password: "password123",
+//       });
+//     expect(res.statusCode).toEqual(201);
+//     expect(res.body.data.role).toEqual("customer");
+//   });
+// });
+
+describe("POST /auth/login-customer", () => {
+  it("returns status code 200 if user logged in successfuly", async () => {
     const res = await request(app)
-      .post("/auth/register-customer")
+      .post("/auth/login-customer")
       .send({
-        first_name: "john",
-        last_name: "doe",
         email: "john.doe@example.com",
         password: "password123",
       });
-    expect(res.statusCode).toEqual(201);
+    expect(res.statusCode).toEqual(200);
     expect(res.body.data.role).toEqual("customer");
+    expect(res.body.token).toBeDefined()
   });
 });
-
 
 // describe('sanity check', () => {
 //   it('should pass', () => {
