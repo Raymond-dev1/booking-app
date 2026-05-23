a006b6665d63f77c1519f2b31f913c541444ecb5
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-05-23 23:48:58 +0100

    fix(schema):drop invite_expires table

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -27,13 +27,12 @@ function generateUniqueString(length: number = 12): string {
 }
 
 export const users = pgTable('users', {
-     id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+    id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
     first_name: t.varchar("first_name", { length: 256 }),
     last_name: t.varchar("last_name", { length: 256 }),
     email: t.varchar().notNull(),
     phone_number: t.varchar("phone_number", {length:15}),
     invite_token:t.varchar("invite_token", {length:256}),
-    invite_expires:t.timestamp("invite_expires"),
     password_hash: t.varchar("password_hash"),
     is_active: t.boolean("is_active").default(true),
     role: rolesEnum().default("guest"),
