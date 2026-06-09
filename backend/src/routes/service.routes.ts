c0ceed3561e552d368cd3d5a6eb6d665a06db291
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-06-09 12:42:18 +0100

    fix(service): add authenticate middleware

--- backend/src/routes/service.routes.ts
+++ backend/src/routes/service.routes.ts
@@ -2,10 +2,10 @@ import {Router} from "express"
 const router:Router =Router()
 
 import {CreateServiceController, DeleteServiceController, DeleteAllServicesController} from "../controllers/service.controller.js";
-import { authorize } from "../middlewares/auth.middleware.js"
+import { authorize, authenticate } from "../middlewares/auth.middleware.js"
 
-router.post("/create",authorize("owner") ,CreateServiceController, )
-router.delete("/delete/:serviceId",authorize("owner") ,DeleteServiceController)
-router.delete("/delete",authorize("owner"), DeleteAllServicesController)
+router.post("/create", authenticate, authorize("owner"), CreateServiceController)
+router.delete("/delete/:serviceId",authenticate, authorize("owner"), DeleteServiceController)
+router.delete("/delete", authenticate, authorize("owner"), DeleteAllServicesController)
 
 export default router;
